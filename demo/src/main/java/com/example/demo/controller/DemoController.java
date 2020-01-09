6181e7970d23fc99f4bad622a39641efa72c81ce
Jason Brown <jbjbro6@gmail.com>
2020-01-08 22:40:18 -0500

    trying to make it look better with spacing

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -137,12 +137,9 @@ public class DemoController {
                 res.append("    ");
             }
             for(int j = 0; j <= i; j++) {
-                if (j == 0 || i == 0)
-                    coef = 1;
-                else
-                    coef = coef * (i - j + 1) / j;
+                j == 0 || i == 0 ? coef = 1 : coef = coef * (i - j + 1) / j;
 
-                res.append("    "); 
+                res.append("     "); 
                 res.append(coef);
             }
             res.append(System.getProperty("line.separator"));
